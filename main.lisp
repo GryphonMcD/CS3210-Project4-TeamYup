18bc3bcb2261d575855d721638aa5235f28bf257
Alexander Gonzalez Ramirez <agonz319@msudenver.edu>
2025-05-04 06:27:40 -0600

    union cons

--- main.lisp
+++ main.lisp
@@ -8,7 +8,7 @@
   (cond
     ((equal set-2 nil) set-1) 
     ((set-member set-1 (car set-2)) (set-union set-1 (cdr set-2))) 
-    (t (set-union (append set-1 (list (car set-2))) (cdr set-2)))))
+    (t (set-union (cons (car set-2) set-1) (cdr set-2)))))
 
 (defun set-intersection (set-1 set-2)
   (cond 

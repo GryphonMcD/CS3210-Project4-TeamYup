4c558b9e842a230522587e1c5b7f2c49c157ccc4
Gryphon McDaniel <89652016+GryphonMcD@users.noreply.github.com>
2025-05-06 19:26:09 -0600

    Cleaning up the file. Creating official main, fiveam_main is now the primary testing main, alternative_main w/ testing is the 'backup'. Use fiveam_main used for demo.

--- difference.lisp
+++
@@ -1,9 +0,0 @@
-;;;;;;;;;;;;;;; ticket 6 / difference set ;;;;;;;;;;;;;;;;
-(defun set-diff (set-1 set-2)
-
-    (let ((item (car set-1)))
-    (if (set-member set-2 item)
-        (set-diff (cdr set-1) set-2)
-        (cons item (set-diff (cdr set-1) set-2))))
-        
-)
--- main.lisp
+++ main.lisp
@@ -3,12 +3,14 @@
     ((equal set nil) nil)
     ((equal (car set) item) t)
     (t (set-member (cdr set) item))))
+    
 
 (defun set-union (set-1 set-2)
   (cond
     ((equal set-2 nil) set-1) 
     ((set-member set-1 (car set-2)) (set-union set-1 (cdr set-2))) 
     (t (set-union (cons (car set-2) set-1) (cdr set-2)))))
+
 
 (defun set-intersection (set-1 set-2)
   (cond 
@@ -17,11 +19,13 @@
     (cons (car set-1) (set-intersection (cdr set-1) set-2))) 
     (t (set-intersection (cdr set-1) set-2))))
 
+
 (defun set-diff (set-1 set-2)
   (cond
     ((equal set-1 nil) nil)
     ((set-member set-2 (car set-1)) (set-diff (cdr set-1) set-2))
     (t (cons (car set-1) (set-diff (cdr set-1) set-2)))))
+
 
 (defun boolean-xor (a b)
   (cond
@@ -29,11 +33,13 @@
     ((or a b) t)
     (t nil)))
 
+
 (defun boolean-implies (a b)
   (or (not a) b))
 
 (defun boolean-iff (a b)
-  (and (boolean-implies a b) (boolean-implies b a)))
+  (NOT (boolean-xor a b)))
+
 
 (defun boolean-eval (exp)
   (cond
@@ -43,20 +49,48 @@
       (NOT(boolean-eval(SECOND exp))))
 
     ((EQUAL (CAR exp) 'and) 
-      (AND(boolean-eval(SECOND exp)) (boolean-eval(THIRD exp))))
+      (AND(boolean-eval(SECOND exp)) 
+          (boolean-eval(THIRD exp))))
 
     ((EQUAL (CAR exp) 'or) 
-      (OR(boolean-eval(SECOND exp)) (boolean-eval(THIRD exp))))
+      (OR(boolean-eval(SECOND exp)) 
+         (boolean-eval(THIRD exp))))
 
     ((EQUAL (CAR exp) 'xor) 
-      (boolean-xor(boolean-eval(SECOND exp)) (boolean-eval(THIRD exp))))
+      (boolean-xor(boolean-eval(SECOND exp)) 
+                  (boolean-eval(THIRD exp))))
 
     ((EQUAL (CAR exp) 'implies) 
-      (boolean-implies(boolean-eval(SECOND exp)) (boolean-eval(THIRD exp))))
+      (boolean-implies(boolean-eval(SECOND exp)) 
+                      (boolean-eval(THIRD exp))))
 
-    ((EQUAL (CAR exp) 'iif) 
-      (boolean-iff(boolean-eval(SECOND exp)) (boolean-eval(THIRD exp))))
+     ((equal (first exp) 'iff)
+     (boolean-iff (boolean-eval (second exp))
+                       (boolean-eval (third exp))))
   )
 )
 
-; Merge Sort
+
+;; Merge sort function w/ helper function
+(defun merge-sort (list predicate)
+  ;; Base case: empty list or single element is already sorted
+  (if (or (null list) (null (cdr list)))
+      list
+      ;; Recursive case: split, sort halves, and merge
+      (let* ((length (length list))
+             (middle (floor length 2))
+             (left-half (subseq list 0 middle))
+             (right-half (subseq list middle)))
+        ;; Recursively sort each half and merge them
+        (merge-lists (merge-sort left-half predicate)
+                     (merge-sort right-half predicate)
+                     predicate))))
+
+(defun merge-lists (list1 list2 predicate)
+  (cond ((null list1) list2)
+        ((null list2) list1)
+        ;; Compare the heads of both lists
+        ((funcall predicate (car list1) (car list2))
+         (cons (car list1) (merge-lists (cdr list1) list2 predicate)))
+        (t
+         (cons (car list2) (merge-lists list1 (cdr list2) predicate)))))
